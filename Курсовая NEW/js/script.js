f627345a53852bf0a4d3d1b71c2fe13a42999a1e
IriTok <noirizh@yandex.ru>
2025-05-30 11:41:08 +0500

    Delete Курсовая NEW directory

--- Курсовая NEW/js/script.js
+++
@@ -1,7 +0,0 @@
-  const hamburger = document.querySelector('.menu_hamburger');
-
-  if (hamburger) {
-    hamburger.addEventListener('click', function(event){
-      event.target.closest('.menu').classList.toggle('menu_active');
-    });
-  }
